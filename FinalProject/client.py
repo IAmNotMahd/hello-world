d20799f39d35541390006f01b02e35a05deaa4e0
IAmNotMahd <38114425+IAmNotMahd@users.noreply.github.com>
2018-05-10 22:15:58 +0000

    Add files via upload

--- FinalProject/client.py
+++ FinalProject/client.py
@@ -1,33 +1,60 @@
+import RPi.GPIO as GPIO
 import socket
 import smbus
 import time
 
+GPIO.setmode(GPIO.BCM)
+
+leftTh = 5
+leftIn = 6
+leftMi = 13
+rightTh = 19
+rightIn = 26
+rightMi = 21
+
+GPIO.setup(leftTh, GPIO.IN)
+GPIO.setup(leftIn, GPIO.IN)
+GPIO.setup(leftMi, GPIO.IN)
+GPIO.setup(rightTh, GPIO.IN)
+GPIO.setup(rightIn, GPIO.IN)
+GPIO.setup(rightMi, GPIO.IN)
+
+#SOCKET + I2C STUFF
 bus = smbus.SMBus(1)
 s = socket.socket()
-port = 12347
+port = 12345
 s.connect(('127.0.0.1', port))
 
 try:
     while True:
-        bus.write_byte_data(0x69, 0x3E, 0x01)
-        bus.write_byte_data(0x69, 0x16, 0x18)
-        data = bus.read_i2c_block_data(0x69, 0x1D, 6)
+        bus.write_byte_data(0x1D, 0x2A, 0x00)
+        bus.write_byte_data(0x1D, 0x2A, 0x01)
+        bus.write_byte_data(0x1D, 0x0E, 0x00)
+        data = bus.read_i2c_block_data(0x1D, 0x00, 7)
 
-        xGyro = data[0] * 256 + data[1]
-        if xGyro > 32767 :
-                xGyro -= 65536
+        xAccl = (data[1] * 256 + data[2]) / 16
+        if xAccl > 2047:
+            xAccl -= 4096
 
-        yGyro = data[2] * 256 + data[3]
-        if yGyro > 32767 :
-                yGyro -= 65536
+        yAccl = (data[3] * 256 + data[4]) / 16
+        if yAccl > 2047:
+            yAccl -= 4096
 
-        zGyro = data[4] * 256 + data[5]
-        if zGyro > 32767 :
-                zGyro -= 65536
+        zAccl = (data[5] * 256 + data[6]) / 16
+        if zAccl > 2047:
+            zAccl -= 4096
         
-        s.sendall(str(xGyro).encode())
+        s.sendall(str(xAccl).encode())
         print("sent x")
-        time.sleep(1)
+        time.sleep(0.01)
+        s.sendall(str(yAccl).encode())
+        print("sent y")
+        time.sleep(0.01)
+        s.sendall(str(zAccl).encode())
+        print("sent z")
+        time.sleep(0.01)
+        
+        time.sleep(0.05)
         
 finally:
     s.close()
--- FinalProject/server.py
+++ FinalProject/server.py
@@ -1,21 +1,157 @@
+import RPi.GPIO as GPIO
 import socket
 import sys
+import time
 
+GPIO.setmode(GPIO.BCM)
+
+inA1 = 16
+pwm1 = 20
+inB1 = 21
+inA2 = 13
+pwm2 = 19
+inB2 = 26
+inA3 = 17
+pwm3 = 27
+inB3 = 22
+
+GPIO.setup(inA1, GPIO.OUT)
+GPIO.setup(pwm1, GPIO.OUT)
+GPIO.setup(inB1, GPIO.OUT)
+GPIO.setup(inA2, GPIO.OUT)
+GPIO.setup(pwm2, GPIO.OUT)
+GPIO.setup(inB2, GPIO.OUT)
+GPIO.setup(inA3, GPIO.OUT)
+GPIO.setup(pwm3, GPIO.OUT)
+GPIO.setup(inB3, GPIO.OUT)
+
+p1 = GPIO.PWM(pwm1, 100)
+p2 = GPIO.PWM(pwm2, 100)
+p3 = GPIO.PWM(pwm3, 100)
+p1.start(50)
+p2.start(50)
+p3.start(50)
+
+GPIO.output(inA1, False)
+GPIO.output(inB1, False)
+GPIO.output(inA2, False)
+GPIO.output(inB2, False)
+GPIO.output(inA3, False)
+GPIO.output(inB3, False)
+
+
+# SOCKET STUFF
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket Successfully Created")
 
-port = 12347
+port = 12345
 s.bind(('', port))
 print("Socket binded to {}".format(port))
 
 s.listen(1)
 print("Socket is listening")
+
+# STOP FUNCTION FOR ALL MOTORS
+def stop1():
+    GPIO.output(inA1, False)
+    GPIO.output(inB1, False)
+def stop2():
+    GPIO.output(inA2, False)
+    GPIO.output(inB2, False)
+def stop3():
+    GPIO.output(inA3, False)
+    GPIO.output(inB3, False)
+
+# CLOCKWISE FUNCTION FOR ALL MOTORS
+def cw1():
+    GPIO.output(inA1, False)
+    GPIO.output(inB1, True)
+def cw2():
+    GPIO.output(inA2, False)
+    GPIO.output(inB2, True)
+def cw3():
+    GPIO.output(inA3, False)
+    GPIO.output(inB3, True)
+
+# ANTICLOCKWISE FUNCTION FOR ALL MOTORS
+def acw1():
+    GPIO.output(inA1, True)
+    GPIO.output(inB1, False)
+def acw2():
+    GPIO.output(inA2, True)
+    GPIO.output(inB2, False)
+def acw3():
+    GPIO.output(inA3, True)
+    GPIO.output(inB3, False)
+
+# STOP 
+def allStop():
+    stop1()
+    stop2()
+    stop3()
+
+# ROTATION CLOCKWISE
+def clock():
+    cw1()
+    cw2()
+    cw3()
+
+# ROTATION ANTICLOCKWISE
+def anticlock():
+    acw1()
+    acw2()
+    acw3()
+    
+# FORWARD ALL DIRECTIONS
+def forward1():
+    stop1()
+    acw2()
+    cw3()
+def forward2():
+    stop2()
+    cw1()
+    acw3()
+def forward3():
+    stop3()
+    acw1()
+    cw2()
+
+# BACKWARD ALL DIRECTIONS
+def backward1():
+    stop1()
+    cw2()
+    acw3()
+def backward2():
+    stop2()
+    acw1()
+    cw3()
+def backward3():
+    stop3()
+    cw1()
+    acw2()
+
+    
+# ------------------------#
+#-------------------------#
 try:
+    c, addr = s.accept()
+    print("Got connection from {}".format(addr))
+        
     while True:
-        c, addr = s.accept()
-        print("Got connection from {}".format(addr))
-        while True:
-            print(c.recv(1024).decode())
-    c.close()
+        x = c.recv(1024).decode()
+        print("X Accl = {}".format(x))
+        time.sleep(0.01)
+        y = c.recv(1024).decode()
+        print("Y Accl = {}".format(y))
+        time.sleep(0.01)
+        z = c.recv(1024).decode()
+        print("Z Accl = {}".format(z))
+        time.sleep(0.01)
+        print("*********************")
+        
+        time.sleep(0.05)
+
 finally:
+    print("Cleaning up")
     c.close()
+    GPIO.cleanup()
